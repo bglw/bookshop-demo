f124fd7d3f18fab1b1d05abf4109543ebcd7f9d2
bigelowcc <liam@cloudcannon.com>
2021-01-19 10:59:15 +1300

    Pushing meta tags

---
+++ components/components/card/card.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let compact = false;
+    export let active = false;
+    export let image;
+    export let title;
+    export let subtitle;
+
+    function toggle() {
+        active = !active;
+    }
+</script>
+<div class="c-card" 
+    class:c-card--compact={compact} 
+    class:is-active={active}
+    on:click={toggle}>
+
+    <img class="c-card__image" src="{ image }" alt="Demo alt">
+
+    <h2 class="c-card__title">
+        { title }
+    </h2>
+
+    <p class="c-card__subtitle">
+        { subtitle }
+    </p>
+
+</div>
